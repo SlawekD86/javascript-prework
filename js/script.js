a81d5519c7a9ef8ee1fbdfbcc56779f02594c18c
Slawek Derabin <slawomirderabin@gmail.com>
2022-12-19 11:38:03 +0100

    ruch komputera

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
-
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -15,3 +13,19 @@ else if (playerInput == 3){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+    computerMove = 'papier';
+}
+else if(randomNumber == 3){
+    computerMove = 'nożyce';
+}
+
+printMessage('ruch komputera: ' + computerMove);
